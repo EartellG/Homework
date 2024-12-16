fa443ef877ecf9ff23439d001c1f5b8fb1a62cfe
Sydneii Green <Sydneygreen19@yahoo.com>
2024-12-16 09:22:47 -0600

    added the Temp Lat

--- Week2/TrueSlice.js
+++ Week2/TrueSlice.js
@@ -4,7 +4,7 @@ console.log(typeof pizzaPlace);
 const numberOfToppings = 6;
 console.log(typeof numberOfToppings);
 console.log(numberOfToppings);
-
+console.log(`Welcome to ${pizzaPlace} we have ${numberOfToppings} toppings`);
 if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
 } else {
